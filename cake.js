e95a7f1d87cf72ea92d5b333ef13ed9d856bb1ec
Gabrieli Martins <gabrielidelimamartins@gmail.com>
2025-01-21 16:33:56 -0300

    Add files via upload

---
+++ cake.js
@@ -0,0 +1,35 @@
+// primeiro dia
+// 5 bolos de chocolate
+// 3 bolos de laranja
+
+// segundo dia
+// 3 bolos de cada
+
+// valores
+// chocolate = 5
+// laranja = 3
+
+// faturamento de cada dia
+
+// variaveis utilizadas
+//valor_chocolate valor_laranja quant_chocolate quant_laranja total_dia1 total_dia2
+
+//primeiro dia
+const quant_chocolate = '5'
+const quant_laranja = '3'
+
+const valor_chocolate = '5'
+const valor_laranja = '3'
+
+var total_dia1 = (5*5) + (3*3)
+console.log (total_dia1)
+
+//segundo dia
+const quantchocolate = '3'
+const quantlaranja = '3'
+
+const valorchocolate = '5'
+const valorlaranja = '3'
+
+var total_dia2 = (3*5) + (3*3)
+console.log (total_dia2)
